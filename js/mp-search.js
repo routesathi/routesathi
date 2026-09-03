08060c7150dd2f472df3621ea7e947bb92d05552
routesathi <routesathi3@gmail.com>
2026-09-03 17:49:47 +0530

    Add files via upload

--- js/mp-search.js
+++ js/mp-search.js
@@ -39,7 +39,8 @@
     { name: 'Somnath', key: 'somnath', aliases: ['somanath', 'somnath temple', 'somnath gujarat'], type: 'Jyotirlinga, coast & pilgrimage', url: 'cities/india/gujarat/somnath.html' },
     { name: 'Gir–Sasan', key: 'gir-sasan', aliases: ['gir', 'sasan gir', 'gir national park', 'gir safari'], type: 'Asiatic lions & official safari', url: 'cities/india/gujarat/gir-sasan.html' },
     { name: 'Ekta Nagar / Statue of Unity', key: 'ekta-nagar', aliases: ['statue of unity', 'ekta nagar', 'kevadia', 'kevadiya'], type: 'Landmark, Narmada & family travel', url: 'cities/india/gujarat/ekta-nagar.html' },
-    { name: 'Rann of Kutch / Dhordo', key: 'rann-of-kutch', aliases: ['rann of kutch', 'white rann', 'white rann of kutch', 'dhordo', 'kutch', 'rann utsav'], type: 'White desert, culture & seasonal travel', url: 'cities/india/gujarat/rann-of-kutch.html' }
+    { name: 'Rann of Kutch / Dhordo', key: 'rann-of-kutch', aliases: ['rann of kutch', 'white rann', 'white rann of kutch', 'dhordo', 'kutch', 'rann utsav'], type: 'White desert, culture & seasonal travel', url: 'cities/india/gujarat/rann-of-kutch.html' },
+    { name: 'Jamnagar', key: 'jamnagar', aliases: ['jam nagar', 'lakhota', 'marine national park jamnagar'], type: 'Lake city, heritage & marine nature', url: 'cities/india/gujarat/jamnagar.html' }
   ];
   const form = document.querySelector('#citySearch');
   const input = document.querySelector('#cityInput');
