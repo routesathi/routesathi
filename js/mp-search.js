9e865f90b25c43dd2cb919f76b4e794c40d306c2
routesathi <routesathi3@gmail.com>
2026-09-09 15:25:35 +0530

    Add files via upload

--- js/mp-search.js
+++ js/mp-search.js
@@ -51,7 +51,8 @@
     { name: 'Mandvi', key: 'mandvi', aliases: ['mandvi kutch', 'mandvi beach', 'mandavi', 'vijay vilas palace', 'vijay vilas', 'mandvi shipyard', 'mandvi shipbuilding', 'topansar lake'], type: 'Royal coast, ships & beach', url: 'cities/india/gujarat/mandvi.html' },
     { name: 'Palitana', key: 'palitana', aliases: ['palitana gujarat', 'shatrunjaya', 'shatrunjay', 'shatrunjaya hill', 'palitana jain temple', 'adinath temple palitana', 'chaumukhji tunk'], type: 'Jain pilgrimage & temple hill', url: 'cities/india/gujarat/palitana.html' },
     { name: 'Little Rann of Kutch', key: 'little-rann-of-kutch', aliases: ['little rann', 'little rann of kutch', 'wild ass sanctuary', 'indian wild ass', 'khur', 'bajana', 'zainabad', 'rann safari'], type: 'Wild ass, birding & salt desert', url: 'cities/india/gujarat/little-rann-of-kutch.html' },
-    { name: 'Velavadar Blackbuck National Park', key: 'velavadar', aliases: ['velavadar', 'velavadar blackbuck', 'blackbuck national park', 'blackbuck park gujarat', 'velavadar national park', 'bhavnagar blackbuck', 'velavadar bird sanctuary'], type: 'Blackbuck grassland & winter birding', url: 'cities/india/gujarat/velavadar.html' }
+    { name: 'Velavadar Blackbuck National Park', key: 'velavadar', aliases: ['velavadar', 'velavadar blackbuck', 'blackbuck national park', 'blackbuck park gujarat', 'velavadar national park', 'bhavnagar blackbuck', 'velavadar bird sanctuary'], type: 'Blackbuck grassland & winter birding', url: 'cities/india/gujarat/velavadar.html' },
+    { name: 'Porbandar', key: 'porbandar', aliases: ['porbandar gujarat', 'kirti mandir porbandar', 'gandhi birthplace', 'sudama mandir', 'porbandar beach', 'porbandar chowpatty', 'porbandar bird sanctuary'], type: 'Gandhi heritage, coast & birding', url: 'cities/india/gujarat/porbandar.html' }
   ];
   const form = document.querySelector('#citySearch');
   const input = document.querySelector('#cityInput');
