6472529476d322c83ae2c632197a7a184d944c46
routesathi <routesathi3@gmail.com>
2026-09-10 17:03:06 +0530

    Add files via upload

--- js/mp-search.js
+++ js/mp-search.js
@@ -53,7 +53,8 @@
     { name: 'Little Rann of Kutch', key: 'little-rann-of-kutch', aliases: ['little rann', 'little rann of kutch', 'wild ass sanctuary', 'indian wild ass', 'khur', 'bajana', 'zainabad', 'rann safari'], type: 'Wild ass, birding & salt desert', url: 'cities/india/gujarat/little-rann-of-kutch.html' },
     { name: 'Velavadar Blackbuck National Park', key: 'velavadar', aliases: ['velavadar', 'velavadar blackbuck', 'blackbuck national park', 'blackbuck park gujarat', 'velavadar national park', 'bhavnagar blackbuck', 'velavadar bird sanctuary'], type: 'Blackbuck grassland & winter birding', url: 'cities/india/gujarat/velavadar.html' },
     { name: 'Porbandar', key: 'porbandar', aliases: ['porbandar gujarat', 'kirti mandir porbandar', 'gandhi birthplace', 'sudama mandir', 'porbandar beach', 'porbandar chowpatty', 'porbandar bird sanctuary'], type: 'Gandhi heritage, coast & birding', url: 'cities/india/gujarat/porbandar.html' },
-    { name: 'Rajkot', key: 'rajkot', aliases: ['rajkot gujarat', 'kaba gandhi no delo', 'gandhi smriti rajkot', 'watson museum', 'jubilee garden rajkot', 'bangdi bazaar', 'rotary dolls museum'], type: 'Gandhi heritage, museums & city culture', url: 'cities/india/gujarat/rajkot.html' }
+    { name: 'Rajkot', key: 'rajkot', aliases: ['rajkot gujarat', 'kaba gandhi no delo', 'gandhi smriti rajkot', 'watson museum', 'jubilee garden rajkot', 'bangdi bazaar', 'rotary dolls museum'], type: 'Gandhi heritage, museums & city culture', url: 'cities/india/gujarat/rajkot.html' },
+    { name: 'Nalsarovar Bird Sanctuary', key: 'nalsarovar', aliases: ['nalsarovar', 'nal sarovar', 'nal sarovar bird sanctuary', 'nalsarovar bird sanctuary', 'nalsarovar lake', 'bird sanctuary near ahmedabad', 'flamingo gujarat', 'pelican gujarat'], type: 'Wetland birding & seasonal nature trip', url: 'cities/india/gujarat/nalsarovar.html' }
   ];
   const form = document.querySelector('#citySearch');
   const input = document.querySelector('#cityInput');
